782c8fa76bf513738d7466f9b4f13d4ed44a5729
Leonid Kvit <1cetoprosto@gmail.com>
2021-06-05 21:38:05 +0300

    внедрил SnapKit в файл DetailCallViewControler

--- b24-reports/Views/Calls/DetailCallViewController.swift
+++ b24-reports/Views/Calls/DetailCallViewController.swift
@@ -9,39 +9,92 @@ import UIKit
 
 class DetailCallViewController: UIViewController {
 
+    //MARK: Private properties
+    private enum UIConstants {
+        static let topInset: CGFloat = 20
+        static let leftRightInset: CGFloat = 16
+        static let spacing: CGFloat = 20
+    }
+    
     var call: Call!
     
-    @IBOutlet weak var managerLabel: UILabel!
-    @IBOutlet weak var dateLabel: UILabel!
-    @IBOutlet weak var qtyInLabel: UILabel!
-    @IBOutlet weak var qtyOutLabel: UILabel!
-    @IBOutlet weak var timeInLabel: UILabel!
-    @IBOutlet weak var timeOutLabel: UILabel!
+    lazy var lblNameManager = UIValueName("Manager")
+    lazy var lblNameDate = UIValueName("Date")
+    lazy var lblNameQtyIn = UIValueName("Q-ty In")
+    lazy var lblNameQtyOut = UIValueName("Q-Ty Out")
+    lazy var lblNameTimeIn = UIValueName("Time In")
+    lazy var lblNameTimeOut = UIValueName("Time Out")
+
+    lazy var lblManager = UILabel()
+    lazy var lblDate = UILabel()
+    lazy var lblQtyIn = UILabel()
+    lazy var lblQtyOut = UILabel()
+    lazy var lblTimeIn = UILabel()
+    lazy var lblTimeOut = UILabel()
+    
+    private lazy var stackView_Main: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [self.stackView_Manager,
+                                                       self.stackView_Date,
+                                                       self.stackView_QtyIn,
+                                                       self.stackView_QtyOut,
+                                                       self.stackView_TimeIn,
+                                                       self.stackView_TimeOut])
+        stackView.axis = .vertical
+        stackView.spacing = UIConstants.spacing
+        
+        return stackView
+    }()
+    
+    private lazy var stackView_Manager = horizontalStackView(subviews: [lblNameManager, lblManager])
+    private lazy var stackView_Date = horizontalStackView(subviews: [lblNameDate, lblDate])
+    private lazy var stackView_QtyIn = horizontalStackView(subviews: [lblNameQtyIn, lblQtyIn])
+    private lazy var stackView_QtyOut = horizontalStackView(subviews: [lblNameQtyOut, lblQtyOut])
+    private lazy var stackView_TimeIn = horizontalStackView(subviews: [lblNameTimeIn, lblTimeIn])
+    private lazy var stackView_TimeOut = horizontalStackView(subviews: [lblNameTimeOut, lblTimeOut])
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        initialize()
         if call != nil {
-            managerLabel.text = "\(call.manager!.firstName) \(call.manager!.lastName)"
-            dateLabel.text = call.dateFormated
-            qtyInLabel.text = String(call.qtyIncomingCalls)
-            qtyOutLabel.text = String(call.qtyOutgoingCalls)
-            timeInLabel.text = String(call.timeOfIncoming)
-            timeOutLabel.text = String(call.timeOfOutgoing)
+            lblManager.text = "\(call.manager!.firstName) \(call.manager!.lastName)"
+            lblDate.text = call.dateFormated
+            lblQtyIn.text = String(call.qtyIncomingCalls)
+            lblQtyOut.text = String(call.qtyOutgoingCalls)
+            lblTimeIn.text = String(call.timeOfIncoming)
+            lblTimeOut.text = String(call.timeOfOutgoing)
         }
-        // Do any additional setup after loading the view.
     }
     
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func initialize() {
+        view.addSubview(stackView_Main)
+        stackView_Main.snp.makeConstraints { maker in
+            maker.top.equalToSuperview().inset(UIConstants.topInset)
+            maker.leading.trailing.equalToSuperview().inset(UIConstants.leftRightInset)
+        }
     }
-    */
-
+    
+    private func horizontalStackView(subviews: [UIView]) -> UIStackView {
+        let stackView = UIStackView(arrangedSubviews: subviews)
+        stackView.axis = .horizontal
+        stackView.spacing = UIConstants.spacing
+        stackView.alignment = .leading
+        stackView.distribution = .equalSpacing
+        
+        return stackView
+    }
 }
+
+class UIValueName: UILabel {
+    
+    init(_ name: String) {
+        super.init(frame: .zero)
+        self.text = name
+        self.textColor = .placeholderText
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
